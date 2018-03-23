b440519fce93d19ee1424522ac74d4a560931a17
google <android@google.com>
2018-03-22 20:27:52 -0700

    fix:sched/fair: Propagate 'struct lb_env' usage into find_busiest_group

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4739,6 +4739,7 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 {
 	int ld_moved, active_balance = 0;
 	struct sched_group *group;
+	unsigned long imbalance;
 	struct rq *busiest;
 	unsigned long flags;
 	struct cpumask *cpus = __get_cpu_var(load_balance_tmpmask);
