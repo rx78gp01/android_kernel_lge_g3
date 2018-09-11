8d6a6fd7381ddc258f2e7e32d4c54347fce4ec9d
Tanya Dixit <tdixit@codeaurora.org>
2018-09-11 17:59:29 +0530

    rtac: Add mutex lock to ensure proper fops access
    Add mutex lock in rtac_open and rtac_release
    to avoid usage count discrepancies leading
    to multiple calls to unmap memory resulting in
    null pointer dereference.
    
    CRs-Fixed: 2271712
    Change-Id: Ie6da28837c352030b8d7e377d68a70cf04e7072a
    Signed-off-by: Tanya Dixit <tdixit@codeaurora.org>

--- sound/soc/msm/qdsp6v2/rtac.c
+++ sound/soc/msm/qdsp6v2/rtac.c
@@ -71,6 +71,7 @@ struct rtac_cal_block_data	rtac_cal[MAX_RTAC_BLOCKS] = {
 struct rtac_common_data {
 	atomic_t			usage_count;
 	atomic_t			apr_err_code;
+	struct mutex			rtac_fops_mutex;
 };
 
 static struct rtac_common_data		rtac_common;
@@ -346,7 +347,9 @@ static int rtac_open(struct inode *inode, struct file *f)
 	int	result = 0;
 	pr_debug("%s\n", __func__);
 
+	mutex_lock(&rtac_common.rtac_fops_mutex);
 	atomic_inc(&rtac_common.usage_count);
+	mutex_unlock(&rtac_common.rtac_fops_mutex);
 	return result;
 }
 
@@ -357,12 +360,15 @@ static int rtac_release(struct inode *inode, struct file *f)
 	int	i;
 	pr_debug("%s\n", __func__);
 
+	mutex_lock(&rtac_common.rtac_fops_mutex);
 	atomic_dec(&rtac_common.usage_count);
 	pr_debug("%s: ref count %d!\n", __func__,
 		atomic_read(&rtac_common.usage_count));
 
-	if (atomic_read(&rtac_common.usage_count) > 0)
+	if (atomic_read(&rtac_common.usage_count) > 0) {
+		mutex_unlock(&rtac_common.rtac_fops_mutex);
 		goto done;
+	}
 
 	for (i = 0; i < MAX_RTAC_BLOCKS; i++) {
 		result2 = rtac_unmap_cal_buffer(i);
@@ -379,6 +385,7 @@ static int rtac_release(struct inode *inode, struct file *f)
 			result = result2;
 		}
 	}
+	mutex_unlock(&rtac_common.rtac_fops_mutex);
 done:
 	return result;
 }
@@ -1432,6 +1439,7 @@ static long rtac_ioctl(struct file *f,
 	s32 result = 0;
 	pr_debug("%s\n", __func__);
 
+	mutex_lock(&rtac_common.rtac_fops_mutex);
 	if (arg == 0) {
 		pr_err("%s: No data sent to driver!\n", __func__);
 		result = -EFAULT;
@@ -1511,6 +1519,7 @@ static long rtac_ioctl(struct file *f,
 		       __func__, cmd);
 	}
 done:
+	mutex_unlock(&rtac_common.rtac_fops_mutex);
 	return result;
 }
 
@@ -1536,6 +1545,7 @@ static int __init rtac_init(void)
 	/* Driver */
 	atomic_set(&rtac_common.usage_count, 0);
 	atomic_set(&rtac_common.apr_err_code, 0);
+	mutex_init(&rtac_common.rtac_fops_mutex);
 
 	/* ADM */
 	memset(&rtac_adm_data, 0, sizeof(rtac_adm_data));
